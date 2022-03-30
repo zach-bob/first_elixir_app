c626d63ba0b8fc08b54d19f322402e9eb4576b5f
Zach Robert <zrobert@mojotech.com>
2022-03-30 14:08:58 -0400

    [feat] add agent in bucket

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,44 @@
+# https://elixir-lang.org/getting-started/mix-otp/agent.html
+
+defmodule KV.Bucket do
+  use Agent
+
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts the `value` for the given `key` in the `bucket`.
+  """
+  def put(bucket, key, value) do
+    # Here is the client code
+    Agent.update(bucket, fn state ->
+      # Here is the server code
+      Map.put(state, key, value)
+    end)
+
+    # Back to the client code
+  end
+
+  @doc """
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    # Function is performed on the Agent's side
+    Agent.get_and_update(bucket, fn dict ->
+      Map.pop(dict, key)
+    end)
+  end
+end
